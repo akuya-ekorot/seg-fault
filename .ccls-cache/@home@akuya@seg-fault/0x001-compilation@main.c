f9f3d6e5ea29144b744b436155555cb76838432d
Akuya Ekorot <akuyaekorot@gmail.com>
2022-12-23 23:06:15 +0300

    compilation files

---
+++ .ccls-cache/@home@akuya@seg-fault/0x001-compilation@main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+* main - entry point of the program.
+* Prints "Hello World" followed by a new line
+*
+* Return: Always 0 (Success)
+*/
+int main(void)
+{
+	printf("Hello World.\n");
+	return (0);
+}
