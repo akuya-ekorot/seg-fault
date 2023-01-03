5613cada640b901926362a064a179e47bbff4313
Akuya Ekorot <akuyaekorot@gmail.com>
2023-01-03 15:20:29 +0300

    start of pointers

---
+++ .ccls-cache/@home@akuya@seg-fault/0x003-functions@average.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * average - returns the average of two floats
+ * @num1: first float
+ * @num2: second float
+ * Return: the average of num1 and num2
+*/
+float average(float num1, float num2)
+{
+	float avg;
+
+	avg = (num1 + num2) / 2;
+
+	return (avg);
+}
---
+++ .ccls-cache/@home@akuya@seg-fault/0x003-functions@main.h
@@ -0,0 +1,1 @@
+float average(float num1, float num2);
